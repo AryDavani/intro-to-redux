90de336bbc79564717bdfb1373551b1320815d5c
Ary Davani <arydavani@gmail.com>
2017-08-02 09:12:53 -0400

    boilerplate hello world

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <h2>Hello World</h2>
+        <h3>Hello World</h3>
+        <h4>Hello World</h4>
+        <h5>Hello World</h5>
+      </div>
+    );
+  }
+}
+
+export default App;
